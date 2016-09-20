b8a7cc8bdc9befd7a2394074061d588d546adec1
Shun Hasegawa <hasegawashun.virgo@googlemail.com>
2016-09-20 20:55:47 +1000

    scripts to run NMDS and start plots were added

--- R/analysis.R
+++ R/analysis.R
@@ -17,13 +17,15 @@ source("R/r1_1_create_summary_tbl.R")
 
 
 # stat and figure
-theme_set(theme_bw())
-source("R/r1_2_1_analysis_total_biomass.R") # anlaysis for total/live/dead biomass
-source("R/r1_2_2_analysis_diversity.R")     # analysis for diversity indices
-source("R/r1_2_3_analysis_pfg.R")           # analysis for pfg ratios
-source("R/r1_2_4_analysis_each_spp.R")      # analysis for dominent spp
+theme_set(theme_bw())                                                 # set ggplot backgroud
+rain_cols <- c("green4", "dodgerblue3", "red", "palegreen", "plum3")  # colors to be used to plot rainfall treatments
 
+source("R/r1_2_1_analysis_total_biomass.R")  # anlaysis for total/live/dead biomass
+source("R/r1_2_2_analysis_diversity.R")      # analysis for diversity indices
+source("R/r1_2_3_analysis_pfg.R")            # analysis for pfg ratios
+source("R/r1_2_4_analysis_each_spp.R")       # analysis for dominent spp
+source("R/r1_2_5_analysis_NMDS.R")           # Community composition (NMDS and PCA)
 
-# Community composition (NMDS and PCA)
-
-
+ 
+# figures
+source("R/r1_3_1_fig_starplot.R")  # starplot

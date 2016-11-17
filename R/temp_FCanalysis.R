eac1b469849e328ff3cd97cf39dfa5cd15520dbb
ShunHasegawa <hasegawashun.virgo@googlemail.com>
2016-11-17 21:45:41 +1100

    fourth corner analysis was added

--- R/temp_FCanalysis.R
+++ R/temp_FCanalysis.R
@@ -1,99 +1,85 @@
 library(mvabund)
 library(lattice)
 
-treat_d <- read.csv("Data/root_trait_smmry_bySpp.csv") %>% 
-  gather(variable, value, -spp) %>% 
-  mutate(type = ifelse(grepl("M$", variable), "M", 
-                       ifelse(grepl("SE$", variable), "SE", "N")),
-         trait = gsub("_M$|_SE$|_N$", "", variable)) %>% 
-  select(-variable) %>% 
-  spread(type, value) %>% 
-  filter(N > 2) %>% 
-  select(-N, -SE) %>% 
-  spread(spp, M) %>% 
-  rename(Axonopus.fissifolius     = Axonopus, 
-         Cynodon.dactlyon         = Cynodon,
-         Eragrostis.curvula       = Eragrostis,
-         Hypochaeris.radicata     = Hypochaeris,
-         Lolium.perenne           = Lolium,      
-         Microlaena.stipoides     = Microlaena, 
-         Paspalum.dilitatum       = Paspalum, 
-         Plantago.lanceolata      = Plantago,
-         Senecio.madagascariensis = Senecio,
-         Setaria.parviflora       = Setaria,
-         Vicia.sativa             = Vicia) %>% 
-  gather(variable, value, -trait) %>% 
-  spread(trait, value)
+summary(trate_d)
+row.names(trate_d) <- treat_spp
+trate_dd <- trate_d[, c("sr_ratio", "Tips", "total_SA")]
+# trate_dd2 <- cbind(trate_dd, y = sample(nrow(trate_dd)))
+# vif(lm(y ~  Forks + sr_ratio + Tips + total_SA + total_L, data = trate_dd2))
+# vif(lm(y ~   sr_ratio + Tips + total_SA, data = trate_dd2))
+scatterplotMatrix(trate_dd)
 
-row.names(treat_d) <- treat_d$variable
-treat_d <- treat_d[, -1]
-trait_spp <- row.names(treat_d)
+# sp_d_16apr <- ab_spp_biom %>% 
+#   filter(year == "2016" & season == "Summer" & treatment != "No.shelter") %>% 
+#   arrange(plot) %>% 
+#   select(-plot, -year, -month, -season, -herb) 
 
 sp_d_16apr <- ab_spp_biom %>% 
-  filter(year == "2016" & season == "Summer" & treatment != "No.shelter") %>% 
+  filter(year == "2013" & season == "Winter" & treatment != "No.shelter") %>% 
   arrange(plot) %>% 
   select(-plot, -year, -month, -season, -herb) 
 
-prsent_spp <- colnames(sp_d_16apr[, -1])[colSums(sp_d_16apr[, -1]) > 0]
-common_spp <- intersect(prsent_spp, trait_spp)
+ab_spp_biom_ed <- ab_spp_biom %>% 
+  filter(treatment != "No.shelter")
+
+rql_m_l <- dlply(ab_spp_biom_ed, .(year, season), function(x){
+  rm(d, sp_d, tr_d, env_d, prsent_spp, common_spp)
+  d <<- x %>% 
+    arrange(plot) %>% 
+    select(-plot, -year, -month, -season, -herb) 
+  prsent_spp <<- colnames(d[, -1])[colSums(d[, -1]) > 0]
+  common_spp <<- intersect(prsent_spp, treat_spp)
+  
+  
+  sp_d  <<-  droplevels(d[, common_spp])
+  sp_d  <<- round(sp_d*100, 0)
+  tr_d  <<-  droplevels(trate_dd[common_spp, ])
+  env_d <<- droplevels(data.frame(d[, "treatment"]))
+  ft <- traitglm(sp_d, env_d, tr_d)
+  rql_res <- anova(ft, nBoot = 999)
+  return(list(model = ft, anv = rql_res))
+})
+
+rql_models <- llply(rql_m_l, function(x) x$model)
+rql_res <- ldply(rql_m_l, function(x){
+  tbl <- x$anv$table
+  cbind(term = row.names(tbl), tbl)
+})
+
+plot(rql_models[[1]])
+plot(rql_models[[2]])
+plot(rql_models[[3]])
+plot(rql_models[[4]])
+plot(rql_models[[5]])
 
 
-sp_d <-  droplevels(sp_d_16apr[, common_spp])
-sp_d <- round(sp_d*100, 0)
-tr_d <-  droplevels(treat_d[common_spp, ])
-env_d <- droplevels(data.frame(sp_d_16apr[, "treatment"]))
-
-ft <- traitglm(sp_d, env_d, tr_d)
-plot(ft)
-plot(ft, which = 2)
-ft$fourth.corner
-anova(ft)
-
-a        = max(abs(ft$fourth.corner))
-colort   = colorRampPalette(c("blue","white","red")) 
-
-mx <- as.matrix(ft$fourth.corner)
-colnames(mx) <- gsub("^.*[.][.]", "",colnames(mx))
-
-plot.4th = levelplot(t(mx), xlab="Environmental Variables",
-                     ylab="Species traits", col.regions=colort(100), at=seq(-a, a, length=100),
-                     scales = list( x= list(rot = 45)))
-print(plot.4th)
-
-names(sp_d_16apr)
-sp_d_16apr %>% 
-  select(treatment, one_of(common_spp)) %>% 
-  gather(variable, value, -treatment) %>% 
-ggplot(., aes(x = variable, y = log(value + 1))) +
-  facet_wrap( ~ treatment) +
-  geom_boxplot() +
-  theme(axis.text.x = element_text(angle = 90))
+plot(rql_models[[1]], which = 2)
+plot(rql_models[[2]], which = 2)
+plot(rql_models[[3]], which = 2)
+plot(rql_models[[4]], which = 2)
+plot(rql_models[[5]], which = 2)
 
 
-ftspp1 <- traitglm(sp_d, env_d)
-plot(ftspp1)
-ftspp1$fourth.corner
+rql_plt_l <- llply(names(rql_models), function(x){
+  m <- rql_models[[x]]
+  a        = max(abs(m$fourth.corner))
+  colort   = colorRampPalette(c("blue","white","red")) 
+  
+  mx <- as.matrix(m$fourth.corner)
+  colnames(mx) <- gsub("^.*[.][.]", "",colnames(mx))
+  
+  plot.4th = levelplot(t(mx), xlab="Environmental Variables",
+                       ylab="Species traits", 
+                       col.regions=colort(100), 
+                       at=seq(-a, a, length=100),
+                       scales = list(x= list(rot = 45)),
+                       main = x)
+  return(plot.4th)
+}) 
 
+print(rql_plt_l[[1]])
+print(rql_plt_l[[2]])
+print(rql_plt_l[[3]])#
+print(rql_plt_l[[4]])
+print(rql_plt_l[[5]])#
 
-
-a        = max( abs(ftspp1$fourth.corner))
-colort   = colorRampPalette(c("blue","white","red")) 
-plot.spp = levelplot(t(as.matrix(ftspp1$fourth.corner)), xlab="Environmental Variables",
-                     ylab="Species traits", col.regions=colort(100), at=seq(-a, a, length=100),
-                     scales = list( x= list(rot = 45)))
-print(plot.spp)
-
-
-
-data(antTraits)
-nrow(antTraits$abund)
-ncol(antTraits$abund)
-nrow(antTraits$env)
-ncol(antTraits$env)
-nrow(antTraits$traits)
-ncol(antTraits$traits)
-
-
-
-
-antTraits$env

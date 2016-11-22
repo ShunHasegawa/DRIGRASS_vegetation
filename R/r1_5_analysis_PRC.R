efbe31dd35cf0a190631cc915fa3458df14e6142
ShunHasegawa <hasegawashun.virgo@googlemail.com>
2016-11-22 14:06:39 +1100

    PRC was performed for winter harvests. Summmry table of PERMANOVA was added

--- R/r1_5_analysis_PRC.R
+++ R/r1_5_analysis_PRC.R
@@ -3,6 +3,8 @@
 # prepare dfs -------------------------------------------------------------
 
 ## sp df
+
+### summer
 sp_summ_df <- ab_spp_biom %>% 
   filter(treatment != "No.shelter" & herb == "Control" & month == 4) %>% 
   mutate(treatment = factor(treatment), year = factor(year)) %>% 
@@ -10,10 +12,30 @@ sp_summ_df <- ab_spp_biom %>%
   select(one_of(spp_names[colSums(.[spp_names]) != 0])) %>% 
   droplevels(.)
 
+### winter
+sp_wint_df <- ab_spp_biom %>% 
+  filter(treatment != "No.shelter" & herb == "Control" & month == 10) %>% 
+  mutate(treatment = factor(treatment), year = factor(year)) %>% 
+  arrange(plot, year) %>% 
+  select(one_of(spp_names[colSums(.[spp_names]) != 0])) %>% 
+  droplevels(.)
+
 
 ## site df
+
+### summer
 site_summ_df <- ab_spp_biom %>%
   filter(treatment != "No.shelter" & herb == "Control" & month == 4) %>% 
+  select(-one_of(spp_names)) %>%
+  mutate(time = paste(year, month.abb[as.numeric(month)], sep = "-"),
+         year = factor(year)) %>% 
+  arrange(plot, year) %>% 
+  droplevels(.)
+
+
+### winter
+site_wint_df <- ab_spp_biom %>%
+  filter(treatment != "No.shelter" & herb == "Control" & month == 10) %>% 
   select(-one_of(spp_names)) %>%
   mutate(time = paste(year, month.abb[as.numeric(month)], sep = "-"),
          year = factor(year)) %>% 
@@ -29,42 +51,39 @@ site_summ_df <- ab_spp_biom %>%
 
 ## tranform data
 pc_df_s <- decostand(sp_summ_df, method = "log")
-plot(prc(pc_df_s, site_summ_df$treatment , site_summ_df$year, site_summ_df))  # eucldien
+pc_df_w <- decostand(sp_wint_df, method = "log")
+plot(prc(pc_df_s, site_summ_df$treatment , site_summ_df$year))
+plot(prc(pc_df_w, site_wint_df$treatment , site_wint_df$year))
 
 
-prc_smmr <- capscale(pc_df_s ~ treatment * year + Condition(year), site_summ_df,  # bray-curtis distance
+## PRC
+prc_smmr <- capscale(pc_df_s ~ treatment * year + Condition(year), site_summ_df,
                     distance = "euclidean")
-prc_smmr_res <- summary(prc_smmr, scaling = 3)
-prc_smmr_site <- data.frame(prc_smmr_res$sites, site_summ_df)
-effect_d <- prc_smmr_site %>% 
-  group_by(year, treatment) %>% 
-  summarise(value = mean(CAP1)) %>% 
-  group_by(year) %>% 
-  mutate(eff = value - value[treatment == "Ambient"], 
-         colval = as.character(mapvalues(treatment, treatment, rain_cols)))
-prc_spp <- prc_smmr_res$species
-range(prc_spp[, 1])
-
-get_prc_fig <- function(){
-  par(mai = c(1, .8, .2, 1.5))
-  plot(eff ~ as.numeric(year), data = effect_d, ylim = c(-3, 3), 
-       axes = F, xlab = "Year", ylab = "Effect", type = "n")
-  axis(side = 1, at = c(1, 2, 3), labels = 2014:2016)
-  axis(side = 2, las = 2)
-  box()
-  d_ply(effect_d, .(treatment), function(x){
-    lines(eff ~ as.numeric(year), data = x, col = colval)
-    points(eff ~ as.numeric(year), data = x, col = colval, pch = 19)
-  })
-  legend("bottomleft", col = rain_cols, legend = levels(effect_d$treatment), pch = 19, bty = "n")
-  sppabund <- colSums(sp_summ_df)
-  axis(side = 4, tck = .03, at = prc_spp[sppabund > 100 , 1], 
-       labels = row.names(prc_spp)[sppabund > 100], las = 2, cex.axis = .7)
-  text(x = 3, y = max(effect_d$eff) * 1.2, labels = "*", cex = 2)
-}
-get_prc_fig()
+prc_wint <- capscale(pc_df_w ~ treatment * year + Condition(year), site_wint_df,
+                     distance = "euclidean")
 
 
+## species score of prc
+prc_sp_smmr <- summary(prc_smmr, scaling = 3)$species
+prc_sp_wint <- summary(prc_wint, scaling = 3)$species
+
+
+### subset abundanct species
+sppabund_smmr <- colSums(sp_summ_df)  # total abundance for each sp in summer
+sppabund_wint <- colSums(sp_wint_df)  # total abundance for each sp in winter
+
+prc_sp_smmr <- prc_sp_smmr[sppabund_smmr > 100 , ]
+prc_sp_wint <- prc_sp_wint[sppabund_wint > 100 , ]
+
+
+## effect size on the site scores of the 1st axis of PRC
+prc_effect_smmr <- get_prc_effect_df(prc_smmr, site_summ_df)
+prc_effect_wint <- get_prc_effect_df(prc_wint, site_wint_df)
+
+
+
+
+# Permanova ---------------------------------------------------------------
 
 
 ## get all permutation; this requires computational power, so use multicores and
@@ -73,20 +92,73 @@ detectCores()                  # number of cores in the current machine
 registerDoParallel(cores = 3)  # register parallel background
 
 
+prc_res_smmr <- anova(prc_smmr, by = "axis", parallel = 3,
+                      permutations = how(within = Within(type = "series"),
+                                         plots  = Plots(strata = site_summ_df$plot, 
+                                                        type = "free"),
+                                         nperm  = 4999))
 
-contr <- how(within = Within(type = "series"),
-             plots  = Plots(strata = site_summ_df$plot, type = "free"),
-             nperm  = 4999)
-prc_res <- anova(prc_smmr, permutations = contr, by = "axis", parallel = 3)
-prc_res
+prc_res_wint <- anova(prc_wint, by = "axis", parallel = 3,
+                      permutations = how(within = Within(type = "series"),
+                                         plots  = Plots(strata = site_wint_df$plot, 
+                                                        type = "free"),
+                                         nperm  = 4999))
+prc_res_smmr
+prc_res_wint
+
+
 
 
 ## Test treatement effect for each year (i.e. permanova for each year) 
-prc_by_year <- llply(2014:2016, function(x){
+prc_by_year_smmr <- llply(2014:2016, function(x){
   spd          <- pc_df_s[site_summ_df$year == x, ]
   sited        <- site_summ_df[site_summ_df$year == x, ]
-  prc_year     <- capscale(spd ~ treatment, sited, distance = "euclidean")
-  prc_year_res <- anova(prc_year, permutations = 9999)
-  return(prc_year_res)
+  prc_year     <- adonis(spd ~ treatment, sited, method = "euclidean", permutations = 9999)
+  return(prc_year)
 }, .parallel = TRUE, .paropts = list(.export = c("pc_df_s", "site_summ_df")))
-prc_by_year
+names(prc_by_year_smmr) <- paste(2014:2016, "Summer", sep = "_")
+
+
+prc_by_year_wint <- llply(2013:2014, function(x){
+  spd          <- pc_df_w[site_wint_df$year == x, ]
+  sited        <- site_wint_df[site_wint_df$year == x, ]
+  prc_year     <- adonis(spd ~ treatment, sited, method = "euclidean", permutations = 9999)
+  return(prc_year)
+}, .parallel = TRUE, .paropts = list(.export = c("pc_df_w", "site_wint_df")))
+names(prc_by_year_wint) <- paste(2013:2014, "Winter", sep = "_")
+
+
+
+
+## merge the above results
+prmnv_res_l <- unlist(list(prc_by_year_smmr, prc_by_year_wint), recursive = FALSE)
+prmnv_res_tbl <- ldply(prmnv_res_l, function(x){
+  atb  <- x$aov.tab
+  Fval <- paste0("F(", atb$Df[1], ",", atb$Df[2], ")", "=", round(atb$F.Model[1], 2))
+  Pval <- atb$`Pr(>F)`[1]
+  ptbl <- data.frame(pseudo_F = Fval, R2 = atb$R2[1], P = Pval)
+  return(ptbl)
+})
+prmnv_res_tbl
+
+
+# Figure ------------------------------------------------------------------
+
+
+range(prc_effect_smmr$eff)
+range(prc_effect_wint$eff)
+
+
+get_prc_fig_smmr <- function(){
+  get_prc_fig(prc_effect_smmr, prc_sp_smmr, ylim = c(-3, 3))
+  text(x = 3, y = max(effect_d$eff) * 1.2, labels = "*", cex = 2)  # add star for the year where significant treatment effects were found
+}
+get_prc_fig_smmr()
+
+
+get_prc_fig(prc_effect_wint, prc_sp_wint, ylim = c(-2, 2))
+
+par(mfrow = c(2, 1))
+get_prc_fig_smmr()
+get_prc_fig(prc_effect_wint, prc_sp_wint, ylim = c(-2, 2))
+
